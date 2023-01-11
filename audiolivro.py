c43ae193ebbcd8110f60f0a8e4354834dfbb7494
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2023-01-11 18:57:10 -0300

    Resolução de erros: agora o programa executa normalmente.

--- audiolivro.py
+++ audiolivro.py
@@ -1,20 +1,19 @@
 import PyPDF2
 import pyttsx3
-
+pdf = 'Alice no País das Maravilhas.pdf'
 #Abre o ebook .pdf
-path = open('Alice no País das Maravilhas.pdf','rb')
+path = open(pdf,'rb')
 pdf_reader = PyPDF2.PdfReader(path)
 
 speak = pyttsx3.init(driverName='sapi5')
 speak.setProperty('rate', 200)
 
 #Percorre as páginas do ebook, extrai o texto e faz a leitura
+
 for pages in range(len(pdf_reader.pages)):
     text = pdf_reader.pages[pages].extract_text()
     speak.say(text)
     speak.runAndWait()
 speak.stop()
 
-sn = input("Você deseja exportar esse áudiolivro para mp3?('s' ou 'n'): ")
-if sn == 's':
-    speak.
+
